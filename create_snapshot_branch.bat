e7283c047db154996a78b1714fb08913cfbfba09
Luca Fiorio <luca.fiorio@outlook.com>
2020-02-12 13:32:09 +0100

    Restructure script to add LFS automatically.

--- create_snapshot_branch.bat
+++ create_snapshot_branch.bat
@@ -2,15 +2,7 @@ set /p serialNumber="Enter the iCub S/N as three digits (e.g. 005): "
 git checkout --orphan sn_%serialNumber%
 git rm -rf . !("create_snapshot_branch.bat")
 git reset create_snapshot_branch.bat
-rm '.gitignore'
-(
-echo *.1 filter=lfs diff=lfs merge=lfs -text
-echo *.pdf filter=lfs diff=lfs merge=lfs -text
-echo *.stl filter=lfs diff=lfs merge=lfs -text
-echo *.step filter=lfs diff=lfs merge=lfs -text
-echo *.dxf filter=lfs diff=lfs merge=lfs -text
-) > .gitattributes
-git add .gitattributes
+del '.gitignore'
 (
 echo *
 echo # Accepted files format
@@ -25,6 +17,15 @@ echo !.*dxf
 ) > .gitignore
 git add .gitignore
 git commit -a -m "Initial commit."
+(
+echo *.1 filter=lfs diff=lfs merge=lfs -text
+echo *.pdf filter=lfs diff=lfs merge=lfs -text
+echo *.stl filter=lfs diff=lfs merge=lfs -text
+echo *.step filter=lfs diff=lfs merge=lfs -text
+echo *.dxf filter=lfs diff=lfs merge=lfs -text
+) > .gitattributes
+git add .gitattributes
+git commit -a -m "Add LFS."
 echo # Snapshot of iCub S/N %serialNumber% > README.md
 git add README.md
 git commit -a -m "Add readme."
