60c5c0136517154eb24e629de4a58f0b0c073499
Luca Fiorio <luca.fiorio@outlook.com>
2020-04-22 19:03:05 +0200

    Update the script to create the subfolders and correct typo.

--- create_snapshot_branch.bat
+++ create_snapshot_branch.bat
@@ -1,8 +1,12 @@
 set /p serialNumber="Enter the iCub S/N as three digits (e.g. 005): "
 git checkout --orphan sn_%serialNumber%
-git rm -rf . !("create_snapshot_branch.bat")
-git reset create_snapshot_branch.bat
-del '.gitignore'
+
+REM Delete master branch inherited files.
+git rm --cached -r .
+del .gitignore
+del README.md
+
+REM Create and commit configuration files.
 (
 echo *
 echo # Accepted files format
@@ -13,7 +17,8 @@ echo !*.pdf
 echo !*.md
 echo !*.stl
 echo !*.step
-echo !.*dxf
+echo !*.dxf
+echo !*/
 ) > .gitignore
 git add .gitignore
 git commit -a -m "Initial commit."
@@ -29,4 +34,24 @@ git commit -a -m "Add LFS."
 echo # Snapshot of iCub S/N %serialNumber% > README.md
 git add README.md
 git commit -a -m "Add readme."
+
+REM Create the subfolders with the associated readme.
+set folderList=(cad-model,electronics,wiring,media,prodoc)
+for %%A in %folderList% do (
+mkdir %%A
+cd %%A
+IF %%A==prodoc (
+    echo ### This folder contains the production documentation. > README.md
+) ELSE (
+    echo ### This folder contains the %%A files. > README.md
+)
+cd ..
+)
+git add .
+git commit -a -m "Add subfolders with the associated readme."
+
+REM  Push the new branch
 git push -u origin sn_%serialNumber%
+
+REM Delete the script from the branch
+start /b "" cmd /c del "%~f0"&exit /b
